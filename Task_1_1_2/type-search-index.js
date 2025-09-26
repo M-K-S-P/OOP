e1efb5ae9bebdf6a483507646b68397b973bc3fb
M-K-S-P <M-K-S-P@users.noreply.github.com>
2025-09-26 19:36:04 +0000

    Deploying to gh-pages from @ M-K-S-P/OOP@a9836972a778388cc6a1b1d147cda227f4fe215f 🚀

---
+++ Task_1_1_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org","l":"Card"},{"p":"org","l":"Deck"},{"p":"org","l":"Game"},{"p":"org","l":"Person"}];updateSearchResults();
