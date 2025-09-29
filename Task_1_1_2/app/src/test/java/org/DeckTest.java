60486f2a89afad10066854b0fc2ccdbb302b8386
MK <miha.kokh@gmail.com>
2025-09-29 20:50:30 +0700

    deck test

--- Task_1_1_2/app/src/test/java/org/DeckTest.java
+++ Task_1_1_2/app/src/test/java/org/DeckTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 public class DeckTest {
@@ -14,6 +16,14 @@ public class DeckTest {
         Deck deck = new Deck();
         deck.generateDeck();
         deck.deckShuffle();
+        Set<String> cardString = new HashSet<String>();
+        Card card;
+        for (int i = 0; i < 52; i++){
+            card = deck.fetchCard(i);
+            card.openCard();
+            cardString.add(card.toString());
+        }
+        assertTrue(cardString.size() == 52);
     }
 
     @Test
